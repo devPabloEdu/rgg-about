f2af58d1cc854928abaa1bc3df800b7d76d708c5
Pablo Eduardo <158394108+devPabloEdu@users.noreply.github.com>
2024-05-23 15:34:04 -0300

    corrigindo bugs versão mobile

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
 function openmenu(){
-    document.getElementById("toolbaricon").style.display ='none'
 
     var navLinks = document.getElementById("menutoolbar")
     navLinks.style.display = "flex"
+
 }
